045f413240bca12a00f301528513d11eacf26242
Nahidjc <nahidjc80@gmail.com>
2021-09-01 21:08:58 +0600

    login and logout

--- loginApp/urls.py
+++ loginApp/urls.py
@@ -5,5 +5,6 @@ from loginApp import views
 app_name = 'login_App'
 urlpatterns = [
     path('login-customer/', views.login_view, name='login_customer'),
-    path('signup-customer/', views.sign_up_view, name='signup_customer')
+    path('signup-customer/', views.sign_up_view, name='signup_customer'),
+    path('logout-customer/', views.logout_view, name='logout')
 ]
--- loginApp/views.py
+++ loginApp/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .forms import CustomerSignUpForm, CustomerLoginForm
 from django.shortcuts import redirect
+from .models import CustomerSignUp
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 def sign_up_view(request):
-
-    return render(request, 'base.html', context={})
+    form = CustomerSignUpForm()
+    if request.method == 'POST':
+        form = CustomerSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            print(user)
+            print("valid form")
+            # username = form.cleaned_data['username']
+            # email = form.cleaned_data['email']
+            # password1 = form.cleaned_data['password1']
+            # password2 = form.cleaned_data['password2']
+            user_profile = CustomerSignUp(user=user)
+            user_profile.save()
+            print(user_profile)
+            return HttpResponseRedirect(reverse('login_App:login_customer'))
+    return render(request, 'loginApp/signup.html', context={'form': form})
 
 
 def login_view(request):
-    print("Login page")
     form = CustomerLoginForm()
-    if request.method == 'GET':
-        print('Get Method')
+    # if request.method == 'GET':
+    #     print('Get Method')
     if request.method == 'POST':
         form = CustomerLoginForm(data=request.POST)
         # username = request.POST['username']
@@ -27,10 +44,14 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('')
-            else:
-                return redirect('account/login-customer')
+                return HttpResponseRedirect(reverse('home'))
 
     else:
         print('Pai nai')
     return render(request, 'loginApp/login.html', context={'form': form})
+
+
+@login_required()
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
