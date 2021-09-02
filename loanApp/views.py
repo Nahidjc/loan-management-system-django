a9f3bec5c701e0ddcafcc3f25a7c5a7f04c5c857
Nahidjc <nahidjc80@gmail.com>
2021-09-02 16:36:50 +0600

    admin page design

--- loanApp/views.py
+++ loanApp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
+@login_required(login_url='/account/login-customer')
 def home(request):
     print('This is a home page')
-    return render(request, 'home.html', context={})
+    return render(request, 'admin/adminbase.html', context={})
 
 
 def LoanPage(request):
---
+++ static/js/admin.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId)
+
+        // Validate that all variables exist
+        if (toggle && nav && bodypd && headerpd) {
+            toggle.addEventListener('click', () => {
+                // show navbar
+                nav.classList.toggle('show')
+                // change icon
+                toggle.classList.toggle('bx-x')
+                // add padding to body
+                bodypd.classList.toggle('body-pd')
+                // add padding to header
+                headerpd.classList.toggle('body-pd')
+            })
+        }
+    }
+
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header')
+
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
+
+    function colorLink() {
+        if (linkColor) {
+            linkColor.forEach(l => l.classList.remove('active'))
+            this.classList.add('active')
+        }
+    }
+    linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+    // Your code to run since DOM is loaded and ready
+});
