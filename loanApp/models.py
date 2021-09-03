b7daff4cb73e253130dc2d8d656da5bb369dd7e6
Nahidjc <nahidjc80@gmail.com>
2021-09-03 09:00:52 +0600

    loan Category add

--- loanApp/models.py
+++ loanApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class loanCategory(models.Model):
+    loan_name = models.CharField(max_length=250)
+    creation_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category_name
