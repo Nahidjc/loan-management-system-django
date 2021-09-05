719ed20ddfc0b96a759919567412f4fe9328b0b7
Nahidjc <nahidjc80@gmail.com>
2021-09-05 14:24:47 +0600

    user dashboard

--- loanApp/urls.py
+++ loanApp/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('loan-request/', views.LoanRequest, name='loan_request'),
     path('loan-payment/', views.LoanPayment, name='loan_payment'),
     path('user-transaction/', views.UserTransaction, name='user_transaction'),
+    path('user-loan-history/', views.UserLoanHistory, name='user_loan_history'),
+    path('user-dashboard/', views.UserDashboard, name='user_dashboard'),
 
 ]
--- loanApp/views.py
+++ loanApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import LoanRequestForm, LoanTransactionForm
-from .models import loanRequest, loanTransaction
+from .models import loanRequest, loanTransaction, CustomerLoan
 from django.shortcuts import redirect
 from django.http import HttpResponseRedirect, HttpResponse
+
+from django.db.models import Sum
 # Create your views here.
 
 
@@ -72,3 +74,32 @@ def UserLoanHistory(request):
     loans = loanRequest.objects.filter(
         customer=request.user.customer)
     return render(request, 'loanApp/user_transaction.html', context={'loans': loans})
+
+
+@login_required(login_url='/account/login-customer')
+def UserDashboard(request):
+
+    requestLoan = loanRequest.objects.all().filter(
+        customer=request.user.customer).count(),
+    # approved = loanRequest.objects.all().filter(status='approved').count(),
+    # rejected = loanRequest.objects.all().filter(status='rejected').count(),
+    totalLoan = CustomerLoan.objects.filter(customer=request.user.customer).aggregate(Sum('total_loan'))[
+        'total_loan__sum'],
+    totalPayable = CustomerLoan.objects.filter(customer=request.user.customer).aggregate(
+        Sum('payable_loan'))['payable_loan__sum'],
+    totalPaid = loanTransaction.objects.filter(customer=request.user.customer).aggregate(Sum('payment'))[
+        'payment__sum'],
+    totalDue = int(totalPayable[0])-int(totalPaid[0]),
+
+    dict = {
+        'request': requestLoan[0],
+        # 'approved': approved[0],
+        # 'rejected': rejected[0],
+        'totalLoan': totalLoan[0],
+        'totalPayable': totalPayable[0],
+        'totalPaid': totalPaid[0],
+        'totalDue': totalDue[0],
+
+    }
+    print(dict)
+    return render(request, 'loanApp/user_dashboard.html', context=dict)
